42d75af118c44d0a381bdf79160404ab74476062
Marco <201912953f@utea.edu.pe>
2022-12-22 17:00:21 -0500

    Obtener información en tiempo real

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,8 +2,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  
-  CollectionReference reportesReference = FirebaseFirestore.instance.collection('Reportes');
+  CollectionReference reportesReference =
+      FirebaseFirestore.instance.collection('Reportes');
+
+  Stream<int> counter() async* {
+    for (int i = 0; i < 10; i++) {
+      yield i;
+      await Future.delayed(const Duration(seconds: 2));
+    }
+  }
+
+  Future<int> getNumber() async {
+    return 1000;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,24 +22,25 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Firebase Firestore"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                reportesReference.get().then((value){
-                  QuerySnapshot collection = value;
-                  List<QueryDocumentSnapshot> docs = collection.docs;
-                  print(docs);
-                });
+      body: StreamBuilder(
+        stream: reportesReference.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snap){
+          if(snap.hasData){
+            QuerySnapshot collection = snap.data;
+            List<QueryDocumentSnapshot> docs = collection.docs;
+            List<Map<String, dynamic>> docsMap = docs.map((e) => e.data() as Map<String, dynamic>).toList();
+            print(docsMap);
+            return ListView.builder(
+              itemCount: docsMap.length,
+              itemBuilder:  (BuildContext context, int index){
+                return ListTile(
+                  title: Text(docsMap[index]["name"]),
+                );
               },
-              child: Text(
-                "Obtener la data",
-              ),
-            ),
-          ],
-        ),
+            );
+          }
+          return Center(child: CircularProgressIndicator(),);
+        }
       ),
     );
   }
