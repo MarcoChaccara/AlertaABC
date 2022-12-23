bc2b3193c5b6ab969aec77b297444c339a186137
Marco <201912953f@utea.edu.pe>
2022-12-22 23:15:19 -0500

    Estilos con widgets personalizados

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:alerta_abc/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +18,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "FireTask",
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme:  GoogleFonts.manropeTextTheme(),
+      ),
       home: HomePage(),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:alerta_abc/ui/general/colors.dart';
+import 'package:alerta_abc/ui/widgets/general_widgets.dart';
+import 'package:alerta_abc/ui/widgets/textfield_search_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -19,29 +22,78 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Firebase Firestore"),
+      backgroundColor: kBrandSecondaryColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 22.0),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(30.0),
+                  bottomLeft: Radius.circular(30.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.05),
+                    blurRadius: 12,
+                    offset: const Offset(4, 4),
+                  ),
+                ],
+              ),
+              child: SafeArea(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Bienvenido, Marco",
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                        color: kBrandPrimaryColor,
+                      ),
+                    ),
+                    Text(
+                      "Lista de los casos",
+                      style: TextStyle(
+                        fontSize: 36.0,
+                        fontWeight: FontWeight.w600,
+                        color: kBrandPrimaryColor,
+                      ),
+                    ),
+                    divider10(),
+                    TextFieldSearchWidget(),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-      body: StreamBuilder(
-        stream: reportesReference.snapshots(),
-        builder: (BuildContext context, AsyncSnapshot snap){
-          if(snap.hasData){
-            QuerySnapshot collection = snap.data;
-            List<QueryDocumentSnapshot> docs = collection.docs;
-            List<Map<String, dynamic>> docsMap = docs.map((e) => e.data() as Map<String, dynamic>).toList();
-            print(docsMap);
-            return ListView.builder(
-              itemCount: docsMap.length,
-              itemBuilder:  (BuildContext context, int index){
-                return ListTile(
-                  title: Text(docsMap[index]["name"]),
-                );
-              },
-            );
-          }
-          return Center(child: CircularProgressIndicator(),);
-        }
-      ),
+
+      // body: StreamBuilder(
+      //   stream: reportesReference.snapshots(),
+      //   builder: (BuildContext context, AsyncSnapshot snap){
+      //     if(snap.hasData){
+      //       QuerySnapshot collection = snap.data;
+      //       List<QueryDocumentSnapshot> docs = collection.docs;
+      //       List<Map<String, dynamic>> docsMap = docs.map((e) => e.data() as Map<String, dynamic>).toList();
+      //       print(docsMap);
+      //       return ListView.builder(
+      //         itemCount: docsMap.length,
+      //         itemBuilder:  (BuildContext context, int index){
+      //           return ListTile(
+      //             title: Text(docsMap[index]["name"]),
+      //           );
+      //         },
+      //       );
+      //     }
+      //     return Center(child: CircularProgressIndicator(),);
+      //   }
+      // ),
     );
   }
 }
